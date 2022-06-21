12ab72b8c6e4c26f7be8931c3c7aaa8fdf273d6e
Mario Valenzuela (wariomvc) <wario.mvc@gmai.com>
2022-06-21 14:24:47 -0500

    Agregado Pagina artistas, HTML y CSS

--- src/js/script.js
+++ src/js/script.js
@@ -7,18 +7,38 @@ document.addEventListener('DOMContentLoaded', function () {
 var scrollingState = false;
 
 function eventListeners() {
+    enlaces = document.querySelectorAll("a[href^='#']");
+    enlaces.forEach(enlace =>{
+        enlace.addEventListener('click',scroller);
+    })
     boton_toartistas = document.querySelector('#boton_toartistas');
     boton_toeventos = document.querySelector('#boton_toeventos');
     boton_tocalendario = document.querySelector('#boton_tocalendario');
 
-    boton_toartistas.addEventListener('click', (e)=>moveTo(e,"artistas"));
-    boton_toeventos.addEventListener('click', (e)=>moveTo(e,"eventos"));
-    boton_tocalendario.addEventListener('click', (e)=>moveTo(e,"calendario"));
+    botonLeft = document.querySelector(".boton__left");
+    console.log(botonLeft);
+    botonLeft.addEventListener('mouseover', listaCardsScroll);
+    //boton_toartistas.addEventListener('click', (e)=>moveTo(e,"artistas"));
+    //boton_toeventos.addEventListener('click', (e)=>moveTo(e,"eventos"));
+    //boton_tocalendario.addEventListener('click', (e)=>moveTo(e,"calendario"));
        
 
     //window.addEventListener('scroll', mousescroolling)
 
-};
+}
+function listaCardsScroll(e){
+    console.log(e.target);
+
+}
+function scroller(e){
+    e.preventDefault();
+    let idSeccionToScroll = e.target.attributes.href.value;
+    let seccionToScroll = document.querySelector(idSeccionToScroll);
+    seccionToScroll.scrollIntoView({
+        behavior: "smooth",
+        block:"start",
+    })
+}
 
 
 
