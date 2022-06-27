93d7ada397d9bfdfe9a8c846475133f700fd1885
Mario Valenzuela (wariomvc) <wario.mvc@gmai.com>
2022-06-27 08:21:19 -0500

    Fix direccion de Scroll

--- src/js/script.js
+++ src/js/script.js
@@ -192,11 +192,11 @@ function eventListeners() {
             lastTouch = e.changedTouches[0].pageX;
         else{
             if(lastTouch < e.changedTouches[0].pageX){
-                lista_cards.scrollLeft += 10;
+                lista_cards.scrollLeft += -10;
                 //console.log("Izquierda")
             }
             else if(lastTouch > e.changedTouches[0].pageX)
-                lista_cards.scrollLeft += -10;
+                lista_cards.scrollLeft += +10;
                 //console.log("derecha");
         }
         lastTouch = e.changedTouches[0].pageX;
