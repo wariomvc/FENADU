34e75718a4f0bcd395e92f87449a05d0b99bc128
Mario Valenzuela (wariomvc) <wario.mvc@gmai.com>
2022-06-27 08:23:32 -0500

    Cambio de velocidad de Scrolling

--- src/js/script.js
+++ src/js/script.js
@@ -192,11 +192,11 @@ function eventListeners() {
             lastTouch = e.changedTouches[0].pageX;
         else{
             if(lastTouch < e.changedTouches[0].pageX){
-                lista_cards.scrollLeft += -2;
+                lista_cards.scrollLeft += -4;
                 //console.log("Izquierda")
             }
             else if(lastTouch > e.changedTouches[0].pageX)
-                lista_cards.scrollLeft += +2;
+                lista_cards.scrollLeft += +4;
                 //console.log("derecha");
         }
         lastTouch = e.changedTouches[0].pageX;
